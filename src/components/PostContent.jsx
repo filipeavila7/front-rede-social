c03c0f0a7cdfc18407a5499ad5622368fbe62eac
filipeavila7 <filipeavila076@gmail.com>
2026-04-22 00:04:45 -0300

    ajustando layout de post

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -5,6 +5,7 @@ import api from "../service/api";
 function PostContent() {
     const { postId } = useParams();
     const [post, setPost] = useState(null)
+    const [like, setLike] = useState(false)
 
     async function getPost() {
         try {
@@ -20,8 +21,23 @@ function PostContent() {
 
     }
 
+    async function getPostLiked() {
+        try {
+            const res = await api.get(`/posts/${postId}/liked`);
+            setLike(res.data || null);
+            console.log(res.data)
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+
+    }
+
     useEffect(() => {
         getPost()
+        getPostLiked()
 
 
     }, [])
@@ -62,18 +78,20 @@ function PostContent() {
                             <p className="dados-post-nome">{post.content}</p>
                             
                             <div className="post-acoes-container">
-                                <button className="acao-post-btn">
-                                    <img src="/like.png" alt="" className="img-acao" />
-                                    <p>{post.likesCount}</p>
+                                <button className={like ? "acao-post-btn-liked" : "acao-post-btn"}>
+                                    <img src={like ? '/liked.png' : '/like.png'} alt="" className="img-acao" />
+                                    <p className={like ? "liked" : "not-liked"}>{ post.likesCount}</p>
                                 </button>
                                 <button className="acao-post-btn">
-                                    <img src="" alt="" className="img-acao" />
+                                    <img src="/comment3.png" alt="" className="img-acao" />
+                                    <p>{post.commentsCount}</p>
                                 </button>
                                 <button className="acao-post-btn">
-                                    <img src="" alt="" className="img-acao" />
+                                    <img src="/share.png" alt="" className="img-acao" />
+                                    <p>0</p>
                                 </button>
                                 <button className="salvar-btn">
-                                    <img src="" alt="" />
+                                    <img className="salve-img" src="/save.png" alt="" />
                                 </button>
                             </div>
                         </div>
