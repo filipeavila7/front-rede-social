1091b9405bc92fc5f15094a6388553374b286075
filipeavila7 <filipeavila076@gmail.com>
2026-04-23 18:52:18 -0300

    mehorando login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,13 +41,26 @@ function Login() {
 
   return (
     <main className="layout">
-      <Fundo />
+      <img className="midia-logo" src="/logo.png" alt="" />
+      <div className="texto-social">
+        <div className="titulo">
+          <h1 className="titulos">Compartilhe.</h1>
+          <h1 className="titulo-a">Inspire-se.</h1>
+          <h1 className="titulos">Conecte-se.</h1>
+        </div>
+        <div className="midia-p">
+            <p>Uma plataforma para artistas e fãs</p>
+            <p>compartilharem ideias,  criações e histórias</p>
+        </div>
+      </div>
       <div className="form-main">
         <div className="container">
           <h1>Login</h1>
           <form className="form" onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label>
+            <div className="input-container">
             <input
+              
               type="email"
               id="email"
               name="email"
@@ -55,26 +68,46 @@ function Login() {
               onChange={(e) => setEmail(e.target.value)}
               required
             />
+            <img src="/mail.png" alt="" className="form-icon" />
+            </div>
 
             <label htmlFor="senha">Senha</label>
-            <input
-              type="password"
-              id="senha"
-              name="senha"
-              value={senha}
-              onChange={(e) => setSenha(e.target.value)}
-              required
-            />
+            <div className="input-container">
+              <input
+                type="password"
+                id="senha"
+                name="senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                required
+              />
+              <img src="/lock.png" alt="" className="form-icon" />
+            </div>
+
+            <div className="acao">
+              <div className="radio">
+                <input className="lembrar" id="lembrar" type="checkbox"/>
+              </div>
+              <p className="acao-texto" >Lembrar de mim</p>
+              <div className="esqueceu">
+                <p className="acao-texto-e">Esqueci minha senha</p>
+              </div>
+            </div>
 
             {erro && <p className="erro">{erro}</p>}
 
             <Botao type="submit" variant="primary" disabled={loading}>
-              {loading ? "Entrando..." : "Enviar"}
+              {loading ? "Entrando..." : "Entrar"}
+              <img className="btn-icon-enter" src="/enter.png" alt="" />
             </Botao>
 
+            <div className="linha"></div>
+
             <Link className="link" to="/cadastro">
-              Não tem conta? Cadastre-se
+              Não tem conta?  <span> Cadastre-se</span> 
             </Link>
+            
+           
           </form>
         </div>
       </div>
