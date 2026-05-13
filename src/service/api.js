a4a1269576d5edd30a71a1ba9306f499a7d73e6d
filipeavila7 <filipeavila076@gmail.com>
2026-05-13 11:58:06 -0300

    variavel para base de api

--- src/service/api.js
+++ src/service/api.js
@@ -4,7 +4,7 @@ import { clearSession } from "../utils/session";
 let isHandlingUnauthorized = false;
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 // adcionar token automaticamnte
